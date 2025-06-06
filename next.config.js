c4703b1820a04b524b4f663fe590c6153e172de0
Alfred <alfredkangjr@gmail.com>
2025-06-07 00:11:21 +0800

    fix: firebase

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.export = {
-  output: "export",
-  images: {
-    remotePatterns: [
-      {
-        hostname: "spfx6ofus.web.app",
-      },
-    ],
-    unoptimized: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export'
 }
+
+module.exports = nextConfig
