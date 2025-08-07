547c75ed019a4baa1989061c34f2f209c2ad0502
Alfred <alfredkangjr@gmail.com>
2025-08-07 23:55:33 +0800

    feat: updated events page, adding mobile responsiveness, new photos, aos

--- components/EventsHero.tsx
+++ components/EventsHero.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export const EventsHero = () => {
   return (
-    <div className="bg-[url(/bkgd2.png)] h-auto bg-contain bg-top bg-center flex flex-col overflow-hidden">
-      <div className="h-32 w-full flex px-8 pt-6">
-        <div className="flex w-1/3 flex-col justify-start">
-          <img src="/coming-soon/TopLeft.png" alt="Top Left Logo" className="w-full" />
-          <div className="flex gap-3 ml-6">
+    <div className="bg-[url(/oriBg.png)] h-auto bg-contain bg-top bg-center flex flex-col overflow-hidden">
+      <div className="h-32 w-full flex lg:flex-row flex-col px-8 pt-6">
+        <div className="flex w-full lg:w-1/3 flex-col lg:items-start items-center justify-start">
+          <img src="/coming-soon/TopLeft.png" alt="Top Left Logo" className="md:w-2/3 w-full" />
+          <div className="flex gap-3 ml-0 lg:ml-6 w-1/3 items-start lg:items-center">
             <Link href="https://www.instagram.com/6ofus2025/" target="_blank">
               <img src="/ig.png" alt="Instagram" className="h-1/4" />
             </Link>
@@ -16,7 +16,7 @@ export const EventsHero = () => {
           </div>
         </div>
 
-        <div className="flex w-1/3 items-top justify-center">
+        <div className="hidden lg:flex w-1/3 items-top justify-center">
           <Link href="/">
             <img
               src="/events/6OFUSCircleLogo.png"
@@ -26,20 +26,23 @@ export const EventsHero = () => {
           </Link>
         </div>
 
-        <div className="flex w-1/3 justify-end">
-          <Link href="/" className="flex justify-end">
+        <div className="hidden lg:flex w-1/3 justify-center lg:justify-end">
+          <Link href="/" className="flex justify-center lg:justify-end">
             <img src="/homeBtn.png" alt="Home"/>
           </Link>
         </div>
       </div>
 
-      <div className="w-1/2 flex m-8 py-10 flex-col justify-center mx-auto">
-        <img src="/events/eventsFlyer.png" alt="Events Flyer"/>
-        <img src="/events/eventsBanner.png" alt="Events Banner"/>
-        <img src="/events/prizes.png" alt="Events Prizes"/>
-        <img src="/events/SPnQR.png" alt="SP and QR Prizes"/>
-        <img src="/events/UR.png" alt="UR Prizes"/>
-        <img src="/events/SR.png" alt="SR Prizes"/>
+      <div className="w-full lg:w-1/2 flex py-10 flex-col justify-center mx-auto">
+        <img className="mx-8 my-28 flex" src="/events/eventsFlyer.png" alt="Events Flyer"/>
+        <img className="mx-8" src="/events/eventsBanner.png" alt="Events Banner" data-aos="fade-up" />
+        <img className="mx-8 my-16" src="/events/prizes.png" alt="Events Prizes" data-aos="fade-up" />
+        <div className="flex lg:flex-row flex-col w-auto lg:w-full mb-16 lg:mx-0 mx-8" data-aos="fade-up">
+          <img className="w-full lg:w-1/2" src="/events/SP.png" alt="SP Prizes" />
+          <img className="w-full lg:w-1/2" src="/events/QR.png" alt="QR Prizes" />
+        </div>
+        <img className="mx-8" src="/events/UR.png" alt="UR Prizes" data-aos="fade-up" />
+        <img className="mx-8" src="/events/SR.png" alt="SR Prizes" data-aos="fade-up" />
       </div>
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,40 +2,39 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer id="footer">
-      <hr className="w-11/12 mx-auto" />
-
-      <section className="flex py-10 px-20 justify-between">
-        <div className="flex pl-7 w-1/4 gap-2 items-center col-span-full xl:col-span-2 justify-start">
+    <footer id="footer" className="bg-white text-black">
+      <section className="flex flex-col lg:flex-row items-center py-10 px-6 md:px-12 lg:px-20 justify-between gap-8 text-sm font-nexa">
+        
+        <div className="order-2 lg:order-1 flex gap-4">
           <Link href="https://www.instagram.com/6ofus2025/" target="_blank">
-            <img className="w-24" src="/ig.png" alt="Instagram" />
+            <img className="w-16 md:w-20" src="/ig.png" alt="Instagram" />
           </Link>
           <Link href="https://www.tiktok.com/@6ofus2025" target="_blank">
-            <img className="w-24" src="/tt.png" alt="TikTok" />
+            <img className="w-16 md:w-20" src="/tt.png" alt="TikTok" />
           </Link>
         </div>
 
-        <div className="w-1/4 col-span-full xl:col-span-2 justify-center items-center flex">
-          <Link href="/" className="font-bold text-xl flex items-center">
+        <div className="order-1 lg:order-2 flex justify-center">
+          <Link href="/" className="flex items-center">
             <img
-                src="/coming-soon/60fus.png"
-                alt="6OFUS Logo"
-                className="lucide lucide-panels-top-left mr-2 h-24"
-              />
+              src="/coming-soon/60fus.png"
+              alt="6OFUS Logo"
+              className="h-20 md:h-24"
+            />
           </Link>
         </div>
 
-        <div className="w-1/4 flex flex-col gap-2 items-end text-right col-span-full xl:col-span-2">
-          <div className="font-bold text-md">
-            Contact us at: 6ofus2025@gmail.com
-          </div>
-          <div className="font-bold text-md">
-            © 2025 6OFUS | All rights reserved
-          </div>
-          <Link href="/" className="font-bold text-md underline text-red-500">
+        <div className="order-3 flex flex-col gap-2 items-center lg:items-end text-center lg:text-right">
+          <div className="font-heavy">Contact us at: 6ofus2025@gmail.com</div>
+          <div className="font-heavy">© 2025 6OFUS | All rights reserved</div>
+          <Link
+            href="/"
+            className="font-heavy underline text-red-500 hover:text-red-700"
+          >
             Credits and Acknowledgements
           </Link>
         </div>
+
       </section>
     </footer>
   );
